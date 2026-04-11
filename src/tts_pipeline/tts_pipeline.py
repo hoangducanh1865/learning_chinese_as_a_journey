603409ff466ac874383cac0c59ad470cf2aa33c0
hoangducanh1865 <hoangducanh.1865@gmail.com>
2026-04-12 00:14:10 +0700

    feat: complete HSK dataset, build transcript preprocessing pipeline

---
+++ src/tts_pipeline/tts_pipeline.py
@@ -0,0 +1,15 @@
+class TTS:
+    def __init__(self):
+        pass
+    
+""" 
+Input: a comprehensive vocab from previous step
+Output: audio of all those words with pre-defined format
+"""
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()
+    pass
---
+++ src/video_generation_pipeline/video_generation_pipeline.py
@@ -0,0 +1,15 @@
+class VideoGenerator:
+    def __init__(self):
+        pass
+    
+""" 
+Input: audio from previous step and timestamp of each meaning of each new word
+Output: a video with visualization of information of that meaning of that word
+"""
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()
+    pass
